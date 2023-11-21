93c72fc192c7c2f350364aa71934c4571dfb5050
chaymturak <lls@gmail.com>
2023-11-21 16:39:13 +0200

    sign in & sign up - Chaym Turak

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,21 @@
+import { Routes, BrowserRouter, Route } from 'react-router-dom'
 import './App.css'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 function App() {
 
   return (
     <>
-      
+
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+
     </>
   )
 }
