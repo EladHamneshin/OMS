50b11f067ccb42a9b1eeacfc1b823950f6c0527b
EvyatarRevach <134207635+EvyatarRevach@users.noreply.github.com>
2023-11-22 22:49:47 +0200

    Update connectDbAdmin.ts

--- server/src/configs/connectDbAdmin.ts
+++ server/src/configs/connectDbAdmin.ts
@@ -1,9 +1,9 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 
-const pool = new Pool();
 
 export const connectToPg = async () => {
+    const pool =await new Pool();
     try {
         await pool.connect();
         console.log('Connected to postgres');
