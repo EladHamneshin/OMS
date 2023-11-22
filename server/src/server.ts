c1e25c1b48e1d0ae45f9d6b70dc75be60f08b9d9
EvyatarRevach <134207635+EvyatarRevach@users.noreply.github.com>
2023-11-22 22:25:56 +0200

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,36 +1,35 @@
 import cors from 'cors';
+import dotenv from 'dotenv';
 import morgan from 'morgan';
 import express from 'express';
-
-import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import { connectDB } from './configs/db.js';
+import { errorHandler, notFound } from './middlewares/errorsMiddleware.js';
 import userRoutes from './routes/userRoutes.js';
-import ordersRoutes from './routes/ordersRoutes.js';
-import connectToDatabase from './configs/connectToMongogoDB.js';
-import { connectToPg } from './configs/connectDbAdmin.js';
+import productRoutes from './routes/productsRoutes.js';
+import categoryRoutes from './routes/categoryRoutes.js';
+import cartRoutes from './routes/cartRoutes.js';
 
 const app = express();
 
-
-
-
+// APP CONFIGS
 dotenv.config();
-
-
-
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
-
-
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use('/api/users', userRoutes);
-app.use('/api/orders', ordersRoutes);
+app.use('/api/users/cart', cartRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/category', categoryRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 const port = process.env.PORT ;
 
+await connectDB();
 
-app.listen(port, async () => {
-  await connectToPg();
-  console.log(`Server is running at port ${port}`);
+app.listen(port, () => {
+  console.log(`server is running at port ${port}`);
 });
-
-export default app;
