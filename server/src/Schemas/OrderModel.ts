77ae25dd04e36e3c06f71660d99250999ecc0fc0
EvyatarRevach <134207635+EvyatarRevach@users.noreply.github.com>
2023-11-23 13:49:19 +0200

    Update OrderModel.ts

--- server/src/Schemas/OrderModel.ts
+++ server/src/Schemas/OrderModel.ts
@@ -1,45 +1,46 @@
 import mongoose from 'mongoose';
-import OrderInterface, { OrderEnum, OrderStatusEnum } from '../types/Order.js';
+import OrderInterface, { OrderStatusEnum, OrderEnum } from '../types/Order.js';
 
-const orderSchema: mongoose.Schema<OrderInterface> = new mongoose.Schema<OrderInterface>({
-
-    cartItems: [{
-        productId: String,
-        name: String,
-        description: String,
-        price: Number,
-        quantity: Number
-    }],
-    userId: String,
-    userName: String,
-    userEmail: String,
-    
-    orderTime: Date,
-    status: {
-        type: String,
-        enum: Object.values(OrderStatusEnum),
-    },
-    totalPrice: Number,
-    shippingDetails: {
-        address: {
-            country: String,
-            city: String,
-            celPhone: Number,
-            street: String,
-            zipCode: Number
+const orderSchema = new mongoose.Schema<OrderInterface>(
+    {
+        cartItems: [
+            {
+                productId: String,
+                name: String,
+                description: String,
+                price: Number,
+                quantity: Number,
+            },
+        ],
+        userId: String,
+        userName: String,
+        userEmail: String,
+        orderTime: Date,
+        status: {
+            type: String, 
+            enum: Object.values(OrderStatusEnum),
         },
-        contactNumber: String,
-        orderType: {
-            type: String,
-            enum: Object.values(OrderEnum),
-        }
-    }
-}
-    , {
-        strict: false
+        totalPrice: Number,
+        shippingDetails: {
+            address: {
+                country: String,
+                city: String,
+                street: String,
+                celPhone: Number,
+                zipCode: Number,
+            },
+            contactNumber: String,
+            orderType: {
+                type: String, 
+                enum: Object.values(OrderEnum),
+            },
+        },
+    },
+    {
+        strict: false,
     }
 );
 
 const orderModel: mongoose.Model<OrderInterface> = mongoose.model<OrderInterface>('orders', orderSchema);
 
-export default orderModel
+export default orderModel;
