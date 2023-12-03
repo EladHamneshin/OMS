27a690ccafaf15781d84ec12c1ec81d76374bbf2
DavidRubin212 <djrubin324@gmail.com>
2023-12-03 15:19:38 +0200

    sunday

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,30 +3,28 @@ import './App.css';
 import SignIn from './pages/SignIn';
 import NavBar from './pages/navBar';
 import Home from './pages/home';
-import OrdersComponent from './pages/Orders';``
+import OrdersComponent from './pages/Orders';
 import Register from './pages/Register';
 import Logout from './components/Logout';
 import Kidnapped from './components/Kidnapped';
-// import Geography from './scenes/geography';
 import Graph from './pages/graphs';
 
- 
 function App() {
-	return (
-		<BrowserRouter>
-			<>
-				<NavBar />
-
+  return (
+    <BrowserRouter>
+      <>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<SignIn />} />
           <Route path="/orders" element={<OrdersComponent />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/logout" element={<Logout/>}/>
-          <Route path="/graph" element={<Graph/>}/>
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/graph" element={<Graph />} />
+          {/* 404 Not Found route should be inside the <Routes> element */}
+          <Route path="*" element={<h1>404 Not Found</h1>} />
         </Routes>
-        <Route path={'*'} element={<h1>404 Not Found</h1>} />
-        <Kidnapped/>
+        <Kidnapped />
       </>
     </BrowserRouter>
   );
