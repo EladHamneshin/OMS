bec3da3bf5554ef9650b70fa88d88b58086c66eb
Evyatar <revachevyatar@gmail>
2023-12-05 14:24:42 +0200

    devops

--- server/src/configs/connectDbAdmin.ts
+++ server/src/configs/connectDbAdmin.ts
@@ -2,16 +2,15 @@ import { config } from 'dotenv';
 import pkg from 'pg';
 const { Pool } = pkg;
 config()
-
 const pool = new Pool();
-
 export const connectToPg = async () => {
+    const res =   await pool.connect();
     try {
-        await pool.connect();
         console.log('Connected to postgres');
     } catch (err) {
         console.error('Error connecting to DB', err);
+    }finally{
+        res.release()
     }
 };
-
 export default pool;
